0b426443aab7a7401e96255d3ca6685b48f31889
Daniel Jader Pellattiero <danieljaderpellattiero@outlook.com>
2023-10-15 11:43:13 +0200

    [M1 - M2 - M3 - M4] Settings fixed.

--- M1/Main.py
+++ M1/Main.py
@@ -3,22 +3,9 @@ from M1 import M1
 
 if __name__ == '__main__':
     begin = time.time()
-    model = M1([True, '1s', '2015-01-01', 3, 3, [10, 5], 4, 200, 100, 19, 0],
-               [
-                'IT0000072618_INTESASANPAOLO',
-                'IT0003128367_ENEL',
-                'IT0003261697_AZIMUT',
-                'IT0003796171_POSTEITALIANE',
-                'IT0003856405_LEONARDO',
-                'IT0004176001_PRYSMIAN',
-                'IT0004965148_MONCLER',
-                'IT0005366767_NEXI',
-                'NL0011585146_FERRARI',
-                'NL0015435975_CAMPARI'
-                ])
+    model = M1([False, '1s', '2015-01-01', 3, 3, [10, 5], 4, 200, 100, 19, 0],
+               ['^KS11'])
     model.run()
     end = time.time()
     time_elapsed = end - begin
-    print(f'Time elapsed: {time_elapsed} seconds')
-    print(f'Time elapsed: {time_elapsed / 60} minutes')
     print(f'Time elapsed: {time_elapsed / 3600} hours')
--- M3/Main.py
+++ M3/Main.py
@@ -3,22 +3,9 @@ from M3 import M3
 
 if __name__ == '__main__':
     begin = time.time()
-    model = M3([True, '1s', '2015-01-01', 3, 3, [10, 5], 4, 50, 128, 4, 0],
-               [
-                   'IT0000072618_INTESASANPAOLO',
-                   'IT0003128367_ENEL',
-                   'IT0003261697_AZIMUT',
-                   'IT0003796171_POSTEITALIANE',
-                   'IT0003856405_LEONARDO',
-                   'IT0004176001_PRYSMIAN',
-                   'IT0004965148_MONCLER',
-                   'IT0005366767_NEXI',
-                   'NL0011585146_FERRARI',
-                   'NL0015435975_CAMPARI'
-               ])
+    model = M3([False, '1s', '2015-01-01', 3, 3, [10, 5], 4, 50, 128, 4, 0],
+               ['^KS11'])
     model.run()
     end = time.time()
     time_elapsed = end - begin
-    print(f'Time elapsed: {time_elapsed} seconds')
-    print(f'Time elapsed: {time_elapsed / 60} minutes')
     print(f'Time elapsed: {time_elapsed / 3600} hours')
